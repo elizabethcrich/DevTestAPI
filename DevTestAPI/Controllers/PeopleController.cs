df8505145aedd03eb5d1160cd2459a3213f677dc
elizabethcrich <elizabeth.crichlow@gmail.com>
2022-12-16 16:30:20 -0500

    Search People

--- DevTestAPI/Controllers/PeopleController.cs
+++ DevTestAPI/Controllers/PeopleController.cs
@@ -17,38 +17,71 @@ namespace DevTestAPI.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetPeople()
+        {
+            return Ok(await GetPeopleInternal());
+        }
+
         [HttpGet("Organizations")]
         public async Task<IActionResult> GetOrganizations()
         {
             return Ok(await _context.Organizations.ToListAsync());
         }
 
-        [HttpGet("{OrganizationId}")]
-        public async Task<IActionResult> GetPeopleByOrgId(int OrganizationId)
+        [HttpGet("{OrgId}")]
+        public async Task<IActionResult> GetPeopleByOrgId(int OrgId)
         {
-            return Ok(await _context.People
-                .Where(p => p.OrganizationId == OrganizationId)
+            var people = await GetPeopleInternal();
+
+            return Ok(people.FindAll(p => p.OrganizationId == OrgId));
+        }
+
+        [HttpGet("Search/{OrgId}")]
+        public async Task<IActionResult> SearchPeople(
+            int OrgId, 
+            string? ExtId = null,
+            string? FName = null,
+            string? LName = null,
+            string? Email = null,
+            string? Phone = null
+            )
+        {
+            var people = await GetPeopleInternal();
+
+            people = people.FindAll(p => p.OrganizationId == OrgId);
+
+            if (ExtId != null)
+                people = people.FindAll(p => p.ExternalPersonId == ExtId);
+
+            if (FName != null)
+                people = people.FindAll(p => p.FirstName?.ToLower() == FName.ToLower());
+
+            if (LName != null)
+                people = people.FindAll(p => p.LastName?.ToLower() == LName.ToLower());
+
+            if (Email != null)
+                people = people.FindAll(p => p.PersonEmails.Any(x => x.Email?.ToLower() == Email.ToLower()));
+
+            if (Phone != null)
+                people = people.FindAll(p => p.PersonPhones.Any(x => x.Phone == Phone));
+
+            return Ok(people);
+        }
+
+        private async Task<List<PersonApi>> GetPeopleInternal()
+        {
+            return await _context.People
                 .Include(p => p.Organization)
                 .Include(p => p.PersonEmails)
                 .Include(p => p.PersonPhones)
                 .Select(p => ConvertPerson(p))
-                .ToListAsync());
+                .ToListAsync();
         }
 
         private static PersonApi ConvertPerson(Person person)
         {
             return new PersonApi(person);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetPeople()
-        {
-            return Ok(await _context.People
-                .Include(p => p.Organization)
-                .Include(p => p.PersonEmails)
-                .Include(p => p.PersonPhones)
-                .Select(p => ConvertPerson(p))
-                .ToListAsync());
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+# DevTest API
+
+## Todo
+- Move connection string to appsettings
+- Move GetOrganizations into OrganizationController
+- Use SQL Server Profiler to check API performance
